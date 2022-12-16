50cef0450ab6a52f29a30cb8f22ecee4808abdd8
Naftagaz21 <atanas.janeshliev@gmail.com>
2022-12-16 17:01:18 +0000

    stuck on p2

--- day09/p2.go
+++ day09/p2.go
@@ -34,75 +34,63 @@ func calculateDistance(x2, y2, x1, y1 int) bool {
 	}
 
 }
-//2597
+
+// 2597
 func main() {
 	input, _ := os.Open("input.txt")
 	defer input.Close()
 
 	head := head{Coordinate{0, 0}}
-	tail1 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail2 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail3 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-
-	tail4 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail5 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail6 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-
-	tail7 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail8 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-	tail9 := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
+	tail := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
 
 	scanner := bufio.NewScanner(input)
 
+	headPositions := make([]Coordinate, 0)
+	//headPositions = append(headPositions, head.location)
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
 		direction := line[0]
 		steps, _ := strconv.Atoi(line[1])
 
+		tails := make([]Coordinate, 10)
 		for i := 0; i < steps; i++ {
-			oldHeadY := head.location.y
-			oldHeadX := head.location.x
-			switch direction {
+			oldHeadX := tails[0].x 
+			oldHeadY := tails[0].y
+
+			switch(direction) {
 			case "U":
-				head.location.y += 1
-				if calculateDistance(head.location.x, head.location.y, tail.location.x, tail.location.y) {
-					tail.location.x = oldHeadX
-					tail.location.y = oldHeadY
-					tail.visitedCoords = append(tail.visitedCoords, Coordinate{oldHeadX, oldHeadY})
-				}
-
+				tails[0].y += 1
 			case "D":
-				head.location.y -= 1
-				if calculateDistance(head.location.x, head.location.y, tail.location.x, tail.location.y) {
-					tail.location.x = oldHeadX
-					tail.location.y = oldHeadY
-					tail.visitedCoords = append(tail.visitedCoords, Coordinate{oldHeadX, oldHeadY})
-				}
-
+				tails[0].y -= 1
 			case "L":
-				head.location.x -= 1
-				if calculateDistance(head.location.x, head.location.y, tail.location.x, tail.location.y) {
-					tail.location.x = oldHeadX
-					tail.location.y = oldHeadY
-					tail.visitedCoords = append(tail.visitedCoords, Coordinate{oldHeadX, oldHeadY})
-				}
-
+				tails[0].x -= 1
 			case "R":
-				head.location.x += 1
-				if calculateDistance(head.location.x, head.location.y, tail.location.x, tail.location.y) {
-					tail.location.x = oldHeadX
-					tail.location.y = oldHeadY
-					tail.visitedCoords = append(tail.visitedCoords, Coordinate{oldHeadX, oldHeadY})
+				tails[0].x += 1
+
+				for idx, coordVal := range(tails) {
+					if idx == 0 {
+						continue
+					}
+					if idx - 1 == 0 {
+						
+					}
+
+					diff 
 				}
 			}
+		}
+
+
 		}
 	}
 
 	mp := make(map[Coordinate]bool, 0)
 	for _, coord := range tail.visitedCoords {
+		//fmt.Println(coord)
+
 		mp[coord] = true
 	}
-
 	fmt.Println(len(mp) + 1)
+	fmt.Println(tail.visitedCoords)
 
 }
