8173bc5192d0535a43b393724a98eca78f847134
Naftagaz21 <atanas.janeshliev@gmail.com>
2022-12-19 15:19:27 +0000

    day 09 done

--- day09/p2.go
+++ day09/p2.go
@@ -23,74 +23,87 @@ type tail struct {
 	visitedCoords []Coordinate
 }
 
-func calculateDistance(x2, y2, x1, y1 int) bool {
-	x_dist := math.Pow(float64(x2-x1), 2)
-	y_dist := math.Pow(float64(y2-y1), 2)
-	distance := math.Round(math.Sqrt(x_dist + y_dist))
-	if distance > 1 {
-		return true
-	} else {
-		return false
+func determineMove(x2, y2, x1, y1 int) (int, int) {
+	//
+	dx := math.Abs(float64(x2 - x1))
+	dy := math.Abs(float64(y2 - y1))
+
+	if dx <= 1 && dy <= 1 {
+		return x1, y1
+	} else if dx >= 2 && dy >= 2 {
+		if y1 < y2 {
+			y1 = y2 - 1
+		} else {
+			y1 = y2 + 1
+		}
+
+		if x1 < x2 {
+			x1 = x2 - 1
+		} else {
+			x1 = x2 + 1
+		}
+	} else if dx >= 2 {
+		if x1 < x2 {
+			x1 = x2 - 1
+		} else {
+			x1 = x2 + 1
+		}
+		y1 = y2
+	} else if dy >= 2 {
+		x1 = x2
+		if y1 < y2 {
+			y1 = y2 - 1
+		} else {
+			y1 = y2 + 1
+		}
 	}
+	return x1, y1
 
 }
 
 // 2597
 func main() {
-	input, _ := os.Open("input.txt")
+	input, _ := os.Open("input2.txt")
 	defer input.Close()
 
-	head := head{Coordinate{0, 0}}
-	tail := tail{Coordinate{0, 0}, make([]Coordinate, 0)}
-
 	scanner := bufio.NewScanner(input)
+	tailPositions := make([]Coordinate, 0)
+	head := head{Coordinate{0, 0}}
+	tails := make([]Coordinate, 9)
 
-	headPositions := make([]Coordinate, 0)
-	//headPositions = append(headPositions, head.location)
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
 		direction := line[0]
 		steps, _ := strconv.Atoi(line[1])
-
-		tails := make([]Coordinate, 10)
 		for i := 0; i < steps; i++ {
-			oldHeadX := tails[0].x 
-			oldHeadY := tails[0].y
-
-			switch(direction) {
+			switch direction {
 			case "U":
-				tails[0].y += 1
+				head.location.y += 1
 			case "D":
-				tails[0].y -= 1
+				head.location.y -= 1
 			case "L":
-				tails[0].x -= 1
+				head.location.x -= 1
 			case "R":
-				tails[0].x += 1
-
-				for idx, coordVal := range(tails) {
-					if idx == 0 {
-						continue
-					}
-					if idx - 1 == 0 {
-						
-					}
-
-					diff 
+				head.location.x += 1
+			}
+			tails[0].x, tails[0].y = determineMove(head.location.x, head.location.y, tails[0].x, tails[0].y)
+			for k := 1; k < len(tails); k++ {
+				tails[k].x, tails[k].y = determineMove(tails[k-1].x, tails[k-1].y, tails[k].x, tails[k].y)
+				if k == 8 {
+					tailPositions = append(tailPositions, Coordinate{tails[k].x, tails[k].y})
 				}
 			}
 		}
 
-
-		}
 	}
 
 	mp := make(map[Coordinate]bool, 0)
-	for _, coord := range tail.visitedCoords {
-		//fmt.Println(coord)
-
+	for _, coord := range tailPositions {
 		mp[coord] = true
 	}
-	fmt.Println(len(mp) + 1)
-	fmt.Println(tail.visitedCoords)
+	fmt.Println(len(tailPositions))
+	fmt.Println(len(mp))
+
+	//fmt.Println(tailPositions)
 
 }
